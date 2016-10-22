0a2a7767d8bb736ca0229753c6dd452c0dec8fd3
Lenne Hendrickx <lennehendrickx@gmail.com>
2016-10-22 21:44:41 +0200

    Test merge

--- src/test/java/RxTest.java
+++ src/test/java/RxTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 import rx.Observable;
 import rx.Observer;
+import rx.Scheduler;
 import rx.observers.TestSubscriber;
 import rx.schedulers.Schedulers;
 import rx.subjects.AsyncSubject;
@@ -11,7 +12,7 @@ public class RxTest {
 
     @Test
     public void TestSubscribeOnWithObservableJust() throws Exception {
-        TestSubscriber<String> testSubscriber = TestSubscriber.create(new SystemOutObserver<>());
+        TestSubscriber<String> testSubscriber = TestSubscriber.create(SystemOutObserver.create());
 
         Observable
                 .just("test1", "test2", "test3")
@@ -26,7 +27,7 @@ public class RxTest {
 
     @Test
     public void TestSubscribeOnWithSubject() throws Exception {
-        TestSubscriber<String> testSubscriber = TestSubscriber.create(new SystemOutObserver<>());
+        TestSubscriber<String> testSubscriber = TestSubscriber.create(SystemOutObserver.create());
 
         PublishSubject<String> subject = PublishSubject.create();
         subject
@@ -59,7 +60,58 @@ public class RxTest {
         testSubscriber.assertValues("test2");
     }
 
+    @Test
+    public void TestOnCompletedDownStream() throws Exception {
+        TestSubscriber<String> testSubscriber = TestSubscriber.create(SystemOutObserver.create());
+        PublishSubject<String> stream1 = PublishSubject.create();
+        PublishSubject<String> stream2 = PublishSubject.create();
+        Observable<String> mergedStream = stream1.mergeWith(stream2);
+        mergedStream.subscribe(testSubscriber);
+
+        stream1.onNext("testStream1_value1");
+        stream2.onNext("testStream2_value1");
+        stream1.onCompleted();
+        stream1.onNext("testStream1_value2");
+        stream2.onNext("testStream2_value2");
+        stream2.onCompleted();
+        stream1.onNext("testStream1_value3");
+        stream2.onNext("testStream2_value3");
+
+        testSubscriber.awaitTerminalEvent();
+        testSubscriber.assertCompleted();
+        testSubscriber.assertNoErrors();
+        testSubscriber.assertValues("testStream1_value1", "testStream2_value1", "testStream2_value2");
+    }
+
+    @Test
+    public void TestChainedStream() throws Exception {
+        TestSubscriber<String> testSubscriber = TestSubscriber.create(SystemOutObserver.create());
+        PublishSubject<String> stream1 = PublishSubject.create();
+        PublishSubject<String> stream2 = PublishSubject.create();
+        Observable<String> mergedStream = stream1.mergeWith(stream2);
+        mergedStream.subscribe(testSubscriber);
+
+        stream1.onNext("testStream1_value1");
+        stream2.onNext("testStream2_value1");
+        stream1.onCompleted();
+        stream1.onNext("testStream1_value2");
+        stream2.onNext("testStream2_value2");
+        stream2.onCompleted();
+        stream1.onNext("testStream1_value3");
+        stream2.onNext("testStream2_value3");
+
+        testSubscriber.awaitTerminalEvent();
+        testSubscriber.assertCompleted();
+        testSubscriber.assertNoErrors();
+        testSubscriber.assertValues("testStream1_value1", "testStream2_value1", "testStream2_value2");
+    }
+
+
     static class SystemOutObserver<T> implements Observer<T> {
+
+        static <K> SystemOutObserver<K> create() {
+            return new SystemOutObserver<>();
+        }
 
         @Override
         public void onCompleted() {
